7a59012db87a7c2ec861f9e8d3189de873f3d0cd
suojy <zhuzhubaobeiok@126.com>
2019-02-04 09:56:49 -0600

    extra

--- student_code.py
+++ student_code.py
@@ -130,6 +130,27 @@ class KnowledgeBase(object):
         # Implementation goes here
         # Not required for the extra credit assignment
 
+
+    def helper(self,fact_rule,string,depth):
+        for each in fact_rule.supported_by:
+            string+="\t"*depth+ "SUPPORTED BY\n"
+            string += "\t"*(depth+1)+"fact：" + str(each[0].statement)
+            if each[0].asserted:
+                string += " ASSERTED\n"
+            string += "\t"*depth+"rule: ("
+
+            for i in range(len(each[1].lhs)):
+                string += str(each[1].lhs[i])
+            string.join(",")
+            string+=")"
+            string+=" -> " + str(each[1].rhs)
+            if each[1].asserted:
+                string += " ASSERTED\n"
+                depth-1
+                return (string,depth)
+            else:
+                self.helper(each[1],string,depth+1)
+
     def kb_explain(self, fact_or_rule):
         """
         Explain where the fact or rule comes from
@@ -142,6 +163,38 @@ class KnowledgeBase(object):
         """
         ####################################################
         # Student code goes here
+        if isinstance(fact_or_rule,Fact):
+            if not self._get_fact(fact_or_rule):
+                return "Fact is not in the KB";
+            string = "fact: "+str(fact_or_rule.statement)
+            #if fact_or_rule.asserted:
+                #string+=" Asserted"
+            if  fact_or_rule.supported_by==[]:
+                return string
+            else:
+                string+="\n"
+                depth=1
+                self.helper(fact_or_rule,string,depth)
+        elif isinstance(fact_or_rule,Rule):
+            if not self._get_rule(fact_or_rule):
+                return "Rule is not in the KB";
+            string = "rule: ("
+            for i in range(len(fact_or_rule.lhs)):
+                string += str(fact_or_rule.lhs[i])
+            string.join(",")
+            string += ")"
+            string+=" -> " + str(fact_or_rule.rhs)
+            if fact_or_rule.asserted:
+                string+=" ASSERTED"
+            if  fact_or_rule.supported_by==[]:
+                return string
+            else:
+                string+="\n"
+                depth=1
+                self.helper(fact_or_rule,string,depth)
+        else:
+            return False
+
 
 
 class InferenceEngine(object):
